b82c28208a5d8b6e17dcce0806570111ebd6417f
Dieubeni <kodjim15@gmail.com>
2022-07-15 15:43:59 +0200

    acceuil

--- resources/views/require/header.blade.php
+++ resources/views/require/header.blade.php
@@ -1,17 +1,23 @@
 <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center">
-      <h1 class="logo me-auto"><a href="index.html">Presento<span>.</span></a></h1>
+      <h1 class="logo me-auto"><a href="index.html">Agence évènementiel<span>.</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt=""></a>-->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
+          <li><a class="nav-link scrollto active" href="#hero">Acceuil</a></li>
+          <li><a class="nav-link scrollto" href="#about">A propos</a></li>
+          <li class="dropdown"><a href="#services"><span>Evènements</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Conférences</a></li>
+              <li><a href="#">Mariages et Baptêmes</a></li>
+              <li><a href="#">Séminaires</a></li>
+              <li><a href="#">Festivals</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto " href="#portfolio">Galéries</a></li>
+          <li><a class="nav-link scrollto" href="#team">Equipe</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -34,6 +40,6 @@
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
-      <a href="#about" class="get-started-btn scrollto">Get Started</a>
+      <a href="#about" class="get-started-btn scrollto">Dévis</a>
     </div>
   </header><!-- End Header -->
